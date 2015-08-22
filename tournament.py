017e6c0b86342b2f7b4543b39cfffb1c6c721314
hikuga <hiteshkgadi@yahoo.com>
2015-08-22 07:44:39 -0400

    implementation for new db schema.

--- tournament.py
+++ tournament.py
@@ -2,7 +2,9 @@
 #
 # tournament.py -- implementation of a Swiss-system tournament
 #
-
+'''
+swiss parings module.
+'''
 import psycopg2
 
 def connect():
@@ -15,56 +17,50 @@ def connect():
         print 'Unable to connect to database tournament'
         return (None, None)
 
-
-def deleteMatches():
-    """Remove all the match records from the database."""
+def runquery(query, res=False, params=None):
+    '''
+    Helper function to run a specified query.
+    '''
+    (executed, results) = (False, None)
     con, cur = connect()
     if con is None or cur is None:
-        return None
+        return (executed, results)
     try:
-        cur.execute('TRUNCATE TABLE matches CASCADE;')
-        con.commit()
-    except:
-        return None
+        if not params:
+            cur.execute(query)
+        else:
+            cur.execute(query, params)
+        if res:
+            #print 'getting results for query: '+query
+            results = cur.fetchall()
+        else:
+            con.commit()
+        executed = True
+    except Exception as err:
+        print 'Exception here: '+str(err)
+        return (False, None)
     finally:
         cur.close()
         con.close()
+    return (executed, results)
+
+def deleteMatches():
+    """Remove all the match records from the database."""
+    return runquery('TRUNCATE TABLE matches CASCADE;')[0]
 
 
 def deletePlayers():
     """Remove all the player records from the database."""
-    con, cur = connect()
-    if con is None or cur is None:
-        return None
-    #if deleteMatches() is None:
-        #return None
-    try:
-        cur.execute('TRUNCATE TABLE players CASCADE;')
-        con.commit()
-    except:
-        return None
-    finally:
-        cur.close()
-        con.close()
+    return runquery('TRUNCATE TABLE players CASCADE;')[0]
+
 
 def countPlayers():
     """Returns the number of players currently registered."""
-    con, cur = connect()
-    if con is None or cur is None:
-        return None
-    try:
-        cur.execute('SELECT count(*) FROM players;')
-        row = cur.fetchone()
-        if row is None:
-            return 0
-        else:
-            return row[0]
-    except:
-        return None
-    finally:
-        cur.close()
-        con.close()
-
+    (executed, results) = runquery('SELECT count(*) from players;', True)
+    if executed and results:
+        return results[0][0]
+    else:
+        return 0
 
 def registerPlayer(name):
     """Adds a player to the tournament database.
@@ -73,21 +69,8 @@ def registerPlayer(name):
     Args:
       name: the player's full name (need not be unique).
     """
-    lastrowid = 0
-    con, cur= connect()
-    if con is None or cur is None:
-        return None
-    try:
-        insStmt = 'INSERT INTO players(name) VALUES(%s);'
-        cur.execute(insStmt, (name, ))
-        con.commit()
-        lastrowid = cur.lastrowid
-        return lastrowid
-    except:
-        return None
-    finally:
-        cur.close()
-        con.close()
+    return runquery('INSERT INTO players(name) VALUES(%s);', False,
+    params=(name, ))[0]
 
 
 def playerStandings():
@@ -101,23 +84,12 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
-    con, cur = connect()
-    if con is None or cur is None:
+    (executed, results) = runquery('''select id, max(name), max(wins), \
+    sum(wins+losses) from playerdetails group by id;''', True)
+    if executed and results:
+        return results
+    else:
         return None
-    query = 'select p.id, p.name, COALESCE(sum(pd.score)/4,0) as wins, count(pd.*) as total from \
-             players p left outer join playerdetails pd on (p.id = pd.id) group by p.id order by wins desc;'
-    try:
-        cur.execute(query)
-        rows = cur.fetchall()
-        if rows is None:
-            return 0
-        else:
-            return rows
-    except:
-        return None
-    finally:
-        cur.close()
-        con.close()
 
 
 def reportMatch(winner, loser):
@@ -126,19 +98,8 @@ def reportMatch(winner, loser):
       winner:  the id number of the player who won
       loser:  the id number of the player who lost
     """
-    con, cur = connect()
-    if con is None or cur is None:
-        return None
-    try:
-        insStmt = 'INSERT INTO matches(Player1, Score1, Player2, Score2) VALUES(%s,%s,%s,%s);'
-        cur.execute(insStmt, (winner, 4, loser, 0))
-        con.commit()
-        return cur.lastrowid
-    except:
-        return None
-    finally:
-        cur.close()
-        con.close()
+    return runquery('INSERT INTO matches(winner, loser) VALUES(%s,%s);',
+    False, params=(winner, loser))[0]
 
 
 def swissPairings():
@@ -154,39 +115,18 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
-    #build a lookup to check re-matches
-    _records = {}
-    con, cur = connect()
-    if con is None or cur is None:
-        return None
-    query = 'select player1, player2 from matches'
-    try:
-        cur.execute(query)
-        for row in cur.fetchall():
-            if row[0] not in _records:
-                _records[row[0]] = set()
-            if row[1] not in _records:
-                _records[row[1]] = set()
-            _records[row[0]].add(row[1])
-            _records[row[1]].add(row[0])
-    except:
-        return None
-    finally:
-        cur.close()
-        con.close()
-
+    results = runquery('''select p1.id, p1.name, p2.id, p2.name \
+    from playerdetails p1 cross join playerdetails p2 where \
+    (p1.id <> p2.id) AND (p1.id, p2.id) not in (select winner, loser \
+    from matches)  AND (p2.id, p1.id) not in (select winner, loser from \
+    matches) order by abs(p1.wins - p2.wins), p1.wins desc, p2.wins desc;'''
+    , True)[1]
     pairs = []
-    pmem = set()
-    pstandings = playerStandings()
-    for ith, playerinfo1 in enumerate(pstandings):
-            if playerinfo1[0] in pmem:
-                continue
-            for jth, playerinfo2 in enumerate(pstandings[ith+1:]):
-                if playerinfo2[0] in pmem or int(playerinfo2[0]) in _records[playerinfo1[0]]:
-                    continue
-                pairs.append((playerinfo1[0], playerinfo1[1], playerinfo2[0], playerinfo2[1]))
-                pmem.add(playerinfo1[0])
-                pmem.add(playerinfo2[0])
-                break
-
+    inpairs = set()
+    for result in results:
+        if result[0] in inpairs:
+            continue
+        pairs.append((result[0], result[1], result[2], result[3]))
+        inpairs.add(result[0])
+        inpairs.add(result[2])
     return pairs
